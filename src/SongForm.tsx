7d0e26f5e840b0d3106e0d7e1ba7c1fe96c2e3d0
John Farrell <johnfarrell@fastmail.com>
2024-09-29 18:36:08 +0100

    form fields not required

--- src/SongForm.tsx
+++ src/SongForm.tsx
@@ -43,7 +43,6 @@ export function SongForm() {
               value={songName}
               onChange={(e) => setSongName(e.target.value)}
               className="mt-1 block w-full bg-gray-700 border border-gray-600 rounded-md shadow-sm py-2 px-3 text-gray-100 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
-              required
             />
           </div>
           <div>
@@ -59,7 +58,6 @@ export function SongForm() {
               value={artistName}
               onChange={(e) => setArtistName(e.target.value)}
               className="mt-1 block w-full bg-gray-700 border border-gray-600 rounded-md shadow-sm py-2 px-3 text-gray-100 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
-              required
             />
           </div>
           <div>
@@ -75,7 +73,6 @@ export function SongForm() {
               value={scoreLink}
               onChange={(e) => setScoreLink(e.target.value)}
               className="mt-1 block w-full bg-gray-700 border border-gray-600 rounded-md shadow-sm py-2 px-3 text-gray-100 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
-              required
             />
           </div>
           <div>
@@ -91,7 +88,6 @@ export function SongForm() {
               value={songLink}
               onChange={(e) => setSongLink(e.target.value)}
               className="mt-1 block w-full bg-gray-700 border border-gray-600 rounded-md shadow-sm py-2 px-3 text-gray-100 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
-              required
             />
           </div>
           <div className="print:hidden">
